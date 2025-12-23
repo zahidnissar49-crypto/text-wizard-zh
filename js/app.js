159fab9310abb8bf79cdf17c4dd334f926758aaa
zahidnissar49-crypto <zahidnissar49@gmail.com>
2025-12-23 12:42:56 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,28 +4,32 @@ var allBtns = document.getElementsByTagName("button");
 
 txtBox.addEventListener("input", function(){
     if (txtBox.value == "") {
-        prev.innerText = "Nothing to preview..."
+        prev.innerText = "Nothing to preview...";
         for (var i = 0; i < allBtns.length; i++) {
-            allBtns[i].style.cursor = "not-allowed";
             allBtns[i].style.backgroundColor = "rgb(218, 218, 218)";
             allBtns[i].style.color = "black";
+            allBtns[i].style.cursor = "not-allowed";
         }
     } else {
         prev.innerText = txtBox.value;
         for (var i = 0; i < allBtns.length; i++) {
-            allBtns[i].style.cursor = "pointer";
             allBtns[i].style.backgroundColor = "blue";
             allBtns[i].style.color = "white";
+            allBtns[i].style.cursor = "pointer";
         }
     }
 })
 
+
 allBtns[0].addEventListener("click", function(){
     txtBox.value = txtBox.value.toUpperCase();
-    prev.innerText = prev.innerText.toLocaleUpperCase();
+    prev.innerText = prev.innerText.toUpperCase();
 })
+
+
 allBtns[1].addEventListener("click", function(){
     txtBox.value = txtBox.value.toLowerCase();
-    prev.innerText = prev.innerText.toLocaleLowerCase();
+    prev.innerText = prev.innerText.toLowerCase();
 })
+
 
